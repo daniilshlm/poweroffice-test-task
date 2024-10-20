c065bd68dcdfe9cf9967e855cb59604cad5fdb54
danshlm <shlemov.daniil.dev@gmail.com>
2024-10-20 13:13:43 +0200

    feat: setup testing libraries and husky pre-commit hook

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+};
