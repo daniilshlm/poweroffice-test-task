a7aa2a5ae792e746831ec5c9420093b47c4cf3b7
danshlm <shlemov.daniil.dev@gmail.com>
2024-10-20 11:54:46 +0200

    init: Next.js setup with Typescript and Sass

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "PowerOffice Test Task",
+  description: "Display of company information from the Brønnøysund Register",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
