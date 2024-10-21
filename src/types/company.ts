2dd043630d55c7cfbf47c7efa461839c40de3b4b
danshlm <shlemov.daniil.dev@gmail.com>
2024-10-21 15:46:51 +0200

    feat: type API response and Redux state

---
+++ src/types/company.ts
@@ -0,0 +1,61 @@
+export interface Company {
+  organisasjonsnummer: string;
+  navn: string;
+  organisasjonsform: {
+    kode: string;
+    beskrivelse: string;
+    links: string[];
+  };
+  registreringsdatoEnhetsregisteret: string;
+  registrertIMvaregisteret: boolean;
+  naeringskode1: {
+    kode: string;
+    beskrivelse: string;
+  };
+  hjelpeenhetskode: {
+    kode: string;
+    beskrivelse: string;
+  };
+  harRegistrertAntallAnsatte: boolean;
+  forretningsadresse: {
+    land: string;
+    landkode: string;
+    postnummer: string;
+    poststed: string;
+    adresse: string[];
+    kommune: string;
+    kommunenummer: string;
+  };
+  stiftelsesdato: string;
+  institusjonellSektorkode: {
+    kode: string;
+    beskrivelse: string;
+  };
+  registrertIForetaksregisteret: boolean;
+  registrertIStiftelsesregisteret: boolean;
+  registrertIFrivillighetsregisteret: boolean;
+  sisteInnsendteAarsregnskap: string;
+  konkurs: string;
+  underAvvikling: boolean;
+  underTvangsavviklingEllerTvangsopplosning: boolean;
+  maalform: string;
+  vedtektsdato: string;
+  vedtektsfestetFormaal: string[];
+  aktivitet: string[];
+  registreringsdatoForetaksregisteret: string;
+  registrertIPartiregisteret: boolean;
+  links: string[];
+}
+
+export interface CompanyState {
+  companies: Company[];
+  loading: boolean;
+  error: string | null;
+  totalItems: number;
+}
+
+export interface FetchCompaniesPayload {
+  search: string;
+  page: number;
+  itemsPerPage: number;
+}
