e37765ec82aeea211b67d403b602d55571f00235
danshlm <shlemov.daniil.dev@gmail.com>
2024-10-21 12:16:36 +0200

    feat: add _App component and global styling

---
+++ src/pages/_App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '@/redux/store';
+import '@/styles/globals.scss';
+
+function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default App;
