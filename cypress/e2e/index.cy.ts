dc4590bd5142970d7cc61c39f2630accfd32d84c
danshlm <shlemov.daniil.dev@gmail.com>
2024-10-22 12:44:42 +0200

    fix: cypress test for homepage

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -1,11 +1,29 @@
 /// <reference types="cypress" />
 
-describe('Main Page', () => {
+describe('Home Page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('should display the correct heading', () => {
-    cy.get('h1').should('contain.text', 'Welcome to the Home Page');
+    cy.get('h1').should('contain.text', 'Company Search');
+  });
+
+  it('should display the theme toggle button', () => {
+    cy.get('.theme-toggle-button').should('exist');
+  });
+
+  it('should have a search bar with input and button', () => {
+    cy.get('input[type="text"]').should('have.attr', 'placeholder', 'Search by name...');
+    cy.get('button').contains('Search').should('exist');
+  });
+
+  it('should display the company list', () => {
+    cy.get('.company-list').should('exist');
+  });
+
+  it('should display pagination', () => {
+    cy.get('.pagination').should('exist');
   });
 });
+
